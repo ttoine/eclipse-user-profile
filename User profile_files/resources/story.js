f0d4271e5e54da1da086bc5a18f226b2b62896e1
Antoine THOMAS <antoine@eclipse.org>
2016-07-22 14:41:14 +0200

    adding an html export

---
+++ User profile_files/resources/story.js
@@ -0,0 +1,376 @@
+var story = {
+ "pages": [
+  {
+   "image": "overview.png",
+   "width": 1109,
+   "links": [
+    {
+     "rect": [
+      157,
+      147,
+      236,
+      193
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      236,
+      147,
+      341,
+      193
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      1004,
+      85,
+      1093,
+      104
+     ],
+     "page": 6
+    }
+   ],
+   "title": "overview",
+   "height": 1484
+  },
+  {
+   "image": "uss.png",
+   "width": 1109,
+   "links": [
+    {
+     "rect": [
+      46,
+      147,
+      157,
+      193
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      236,
+      147,
+      341,
+      193
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      1004,
+      85,
+      1093,
+      104
+     ],
+     "page": 6
+    }
+   ],
+   "title": "uss",
+   "height": 1396
+  },
+  {
+   "image": "account-publicprofile.png",
+   "width": 1109,
+   "links": [
+    {
+     "rect": [
+      46,
+      147,
+      157,
+      193
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      157,
+      147,
+      236,
+      193
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      172,
+      217,
+      277,
+      258
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      277,
+      217,
+      355,
+      258
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      355,
+      217,
+      488,
+      258
+     ],
+     "page": 5
+    },
+    {
+     "rect": [
+      986,
+      85,
+      1093,
+      104
+     ],
+     "page": 6
+    }
+   ],
+   "title": "account-publicprofile",
+   "height": 1652
+  },
+  {
+   "image": "account-settings.png",
+   "width": 1109,
+   "links": [
+    {
+     "rect": [
+      46,
+      147,
+      157,
+      193
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      157,
+      147,
+      236,
+      193
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      36,
+      217,
+      172,
+      258
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      277,
+      217,
+      355,
+      258
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      355,
+      217,
+      488,
+      258
+     ],
+     "page": 5
+    },
+    {
+     "rect": [
+      986,
+      85,
+      1093,
+      104
+     ],
+     "page": 6
+    }
+   ],
+   "title": "account-settings",
+   "height": 1156
+  },
+  {
+   "image": "account-eca.png",
+   "width": 1109,
+   "links": [
+    {
+     "rect": [
+      46,
+      147,
+      157,
+      193
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      157,
+      147,
+      236,
+      193
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      36,
+      217,
+      172,
+      258
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      172,
+      217,
+      277,
+      258
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      355,
+      217,
+      488,
+      258
+     ],
+     "page": 5
+    },
+    {
+     "rect": [
+      986,
+      85,
+      1093,
+      104
+     ],
+     "page": 6
+    }
+   ],
+   "title": "account-eca",
+   "height": 2020
+  },
+  {
+   "image": "account-notifications.png",
+   "width": 1109,
+   "links": [
+    {
+     "rect": [
+      46,
+      147,
+      157,
+      193
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      157,
+      147,
+      236,
+      193
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      36,
+      217,
+      172,
+      258
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      172,
+      217,
+      277,
+      258
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      277,
+      217,
+      355,
+      258
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      986,
+      85,
+      1093,
+      104
+     ],
+     "page": 6
+    }
+   ],
+   "title": "account-notifications",
+   "height": 812
+  },
+  {
+   "image": "sign-in.png",
+   "width": 1109,
+   "links": [
+    {
+     "rect": [
+      501,
+      656,
+      673,
+      680
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      410,
+      531,
+      764,
+      575
+     ],
+     "page": 0
+    }
+   ],
+   "title": "sign-in",
+   "height": 812
+  },
+  {
+   "image": "create-an-account.png",
+   "width": 1109,
+   "links": [
+    {
+     "rect": [
+      1004,
+      85,
+      1093,
+      104
+     ],
+     "page": 6
+    },
+    {
+     "rect": [
+      618,
+      1021,
+      967,
+      1065
+     ],
+     "page": 2
+    }
+   ],
+   "title": "create-an-account",
+   "height": 1124
+  }
+ ],
+ "title": "User profile",
+ "highlightLinks": false
+}
